215bb9a36b11670ac7d0d8f63ce47a4c4e1f5a0e
Elena <emmettdiez97@icloud.com>
2024-06-18 13:19:14 +0200

    Update Act3_FSP.R
    Hola! Adjunto el análisis del PCA completo junto con la normalidad. Siguiente paso: tabla con gtsummary.

--- versions /Act3_FSP.R
+++ versions /Act3_FSP.R
@@ -1,101 +1,174 @@
 rm(list=ls())
 
-library(dplyr)   # Para realizar operaciones con dataframes y trabajar con pipes
+# Librerías necesarias
+
+library(nortest)
+library(dplyr)   
 library(tidyr)
 library(tidyverse)
+library(stats)
+library(psych)
+library(factoextra)
+library(gtsummary)
+library(devtools)
+library(gt)
 
-# Importación y procesado de datos
+## PRUEBAS DE NORMALIDAD
+# Importar los datos 
+
 data_act3 <- read.csv("mubio02_act3_alimentos_nutrientes_4900.csv",
-                      header = TRUE, stringsAsFactors = FALSE, sep = ",") %>%
+                      header = TRUE, stringsAsFactors = TRUE, sep = ",") %>%
   subset(select = -id)
 
-# Convertir los valores se las columnas según el tipo de variable (numérica o categórica)
-data_act3 <- data_act3 %>%
-  mutate(ECV_prev = as.factor(sexo)) %>%
-  mutate_at(vars(estado_civil:cancer_prev), as.factor)
-data_act3 <- data_act3 %>%
-  mutate_at(vars(altura:IMC), as.numeric) %>%
-  mutate(edad = as.numeric(edad)) %>%
-  mutate_at(vars(METs_h_semana:nutriente19), as.numeric)
+# Comprobamos la normalidad de los datos usando el test de Anderson-Darling realizando un bucle.
 
-alimentos_nutrientes <- data_act3[,27:ncol(data_act3)]
+pvalor_nutrientes <- matrix(NA, nrow = 19, ncol = 1) # Matriz vacía con 19 filas porque tenemos 19 variables y una columna
+columnas_nutrientes <- c("nutriente1", "nutriente2", "nutriente3", "nutriente4", "nutriente5",
+                       "nutriente6", "nutriente7", "nutriente8", "nutriente9", "nutriente10", 
+                       "nutriente11", "nutriente12", "nutriente13", "nutriente14", "nutriente15", 
+                       "nutriente16", "nutriente17", "nutriente18", "nutriente19")
 
-# Estandarización de datos antes de PCA
-func_estandarizacion <- function(df) {
-  # Aplicar la estandarización a cada columna
-  df_estandarizado <- as.data.frame(lapply(df, function(col) {
-    # Operación de normalización
-    (col - mean(col, na.rm = TRUE)) / sd(col, na.rm = TRUE)
-  }))
-  return(df_estandarizado)
+for (i in 1:length(columnas_nutrientes)) {
+  ad_test_result_nutrientes <- ad.test(data_act3[[columnas_nutrientes[i]]])
+  pvalor_nutrientes[i, ] <- ad_test_result_nutrientes$p.value
 }
 
-alimentos_nutrientes_st <- func_estandarizacion(alimentos_nutrientes)
+pvalor_nutrientes #p-valor <0.001 = no cumple la normalidad, ni de manera conjunta ni separada
 
-# Extracción de los componentes principales
-### OPCIÓN 1 ###
-library(FactoMineR)
-library(factoextra)
-pca_result <- PCA(alimentos_nutrientes)
-head(pca_result$eig) # comprobar los eigenvalores de cada componente principal
 
-### OPCIÓN 2 ### ES LA QUE YO PONDRÍA
-library(stats) # para realizar PCA
-data_pca <- as.matrix(alimentos_nutrientes_st)
-pca_result <- prcomp(t(data_pca), scale. = TRUE)
-print(summary(pca_result)) # Muestra los valores de las componentes principales
+pvalor_alimentos <- matrix(NA, nrow = 131, ncol = 1) # Matriz vacía con 20 filas porque tenemos 20 variables y una columna
+columnas_alimentos <- paste0("alimento", 1:131)
+
+for (i in 1:length(columnas_alimentos)) {
+  ad_test_result_alimentos <- ad.test(data_act3[[columnas_alimentos[i]]])
+  pvalor_alimentos[i, ] <- ad_test_result_alimentos$p.value
+}
+
+pvalor_alimentos #p-valor <0.001 = no cumple la normalidad, ni de manera conjunta ni separada
+
+#############################################################################
+###################### ANALISIS PCA #########################################
+#############################################################################
+
+# Seleccionar las columnas relevantes (alimento y nutriente)
+
+alimentos_nutrientes <- data_act3 %>% 
+  select(starts_with("alimento"), starts_with("nutriente"))
+
+# Comprobar si existen valores nulos
+
+any(is.na(alimentos_nutrientes)) 
+
+# Análisis de PCA + estandarización de los datos mediante la función scale
+
+pca_result <- prcomp (alimentos_nutrientes, center=TRUE, scale = TRUE)
+
+# Visualizar los resultados de los componentes
+
+summary(pca_result)
 
 # Evaluamos la adecuación de los datos para PCA según el criterio de KMO
-### NO CONSIGO QUE FUNCIONE ESTO. LO PODEMOS QUITAR SI NO FUNCIONA
-# library(psych)
-# KMO(r = t(data_pca))
-# print(kmo_result)  # Aceptable según el criterio de KMO
+# Correlacion spearman para poder meter la matriz en el KMO. 
+# Usamos Spearman porque sale mejor coeficiente de KMO (sale 0.86)
+
+corr_an <-cor(alimentos_nutrientes, method="spearman")
+resultado_KMO <- KMO(r=corr_an)
+resultado_KMO
+
+#Grafico scree plot para ver qué factores aportan más.
+fviz_eig(pca_result, addlabels = TRUE, ylim = c(0, 15))
+
+# Visualizar la importancia de cada variable por dimensiones
+
+fviz_contrib(pca_result, choice = "var", axes = 1, top = 10)
+fviz_contrib(pca_result, choice = "var", axes = 2, top = 10)
+
+# Visualizar el gráfico de correlación variable (muestra las relaciones entre todas las variables)
+
+var <- get_pca_var(pca_result) # varianza
+
+fviz_pca_var(pca_result, axes = c(1, 2), 
+             col.var = "cos2", 
+             gradient.cols=c("blue", "yellow", "red"))
 
 
-# Representación gráfica de los dos componentes principales de PCA
-### OPCIÓN 1 ### ESTA ES LA ÚNICA QUE CONSIGO QUE ME QUEDE BIEN
-plot(pca_result$x[,1], pca_result$x[,2]) # Representación de los dos componentes principales
+# Gráfica de los dos componentes principales de PCA.
+# Representación de los dos componentes principales.
 
-### OPCIÓN 2 ###
-plot <- fviz_pca_biplot(pca_result,
+plot_1 <- plot(pca_result$x[,1], pca_result$x[,2])
+
+PCA_Biplot <- fviz_pca_biplot(pca_result, axes = c(1,2),
                         geom.ind = "point",
                         col.ind = "black",
-                        pointsize = 2,
+                        pointsize = 0.5,
                         addEllipses = TRUE,
-                        alpha.var = "contrib",
-                        col.var = "contrib",
-                        repel = TRUE,
+                        alpha.var = "cos2",
+                        col.var = "cos2"
 )
-plot
 
-# Porcentaje de varianza para cada componente principal
+PCA_Biplot
+
+# % de la varianza explicada por los componentes del pca:
+
 pca.var <- pca_result$sdev^2
-pca.var.per <- round(pca.var/sum(pca.var)*100, 1)
-pca.var.per
+pca.var.porcentaje <- round(pca.var/sum(pca.var)*100, 1)
+pca.var.porcentaje
 
-# Obtenemos un "Scree plot" con la contribuición de los componentes principales
-barplot(
-  pca.var.per,main = "% varianza por componente principal",
-  xlab = "Componente principal",
-  ylab = "Porcentaje de varianza total")
+#Consultamos las cargas de cada componente (%) para los 4 más importantes:
+
+head((pca_result$rotation[,1]))
+
+# Podemos hacer biplot (ya hecho arriba) de pca_result con los componentes principales.
+# Vemos representadas las cargas de cada componente y por tanto su relación o influencia.
+
+biplot (pca_result)
 
 # Rotación de cargas
+
+# Para pc1 qué alimentos/nutrientes contribuyen más a la loc de los datos en PCA.
+# Es decir, extraigo las cargas del PC1.
+
 cargas_pc1 <- pca_result$rotation[,1]
-food_scores_pc1 <- abs(cargas_pc1)
-food_scores_pc1_sorted <- sort(food_scores_pc1, decreasing = TRUE)
-food_scores_pc1_top <- food_scores_pc1_sorted[1:10]
-print(food_scores_pc1_top)
+
+# Con abs le digo que me de el valor absoluto de las cargas.
+
+cargaPC1_scores <- abs(cargas_pc1)
+
+# Ordeno los alimentos/nutrientes (cargas) de mayor a menor 
+
+cargaPC1_scores_ordenado <- sort(cargaPC1_scores, decreasing = TRUE)
+
+# Saco los 10 alimentos/nutrientes que más aportan (top 10) para PC1:
+
+top_10_PC1 <- names(cargaPC1_scores_ordenado [1:10])
+
+# Visualización del top 10 alimentos/nutrientes para PC1:
+
+top_10_PC1
+
+# Repito para PC2
 
 cargas_pc2 <- pca_result$rotation[,2]
-food_scores_pc2 <- abs(cargas_pc2)
-food_scores_pc2_sorted <- sort(food_scores_pc2, decreasing = TRUE)
-food_scores_pc2_top <- food_scores_pc2_sorted[1:10]
-print(food_scores_pc2_top)
 
-print(pca_result$rotation[,1]) # Cargas de cada variable para el componente 1
-print(pca_result$rotation[,2]) # Cargas de cada variable para el componente 2
+# Con abs le digo que me de el valor absoluto de las cargas.
+
+cargaPC2_scores <- abs(cargas_pc2)
+
+# Ordeno los alimentos/nutrientes (cargas) de mayor a menor 
+
+cargaPC2_scores_ordenado <- sort(cargaPC2_scores, decreasing = TRUE)
+
+# Saco los 10 alimentos/nutrientes que más aportan (top 10) para PC1:
+
+top_10_PC2 <- names(cargaPC2_scores_ordenado [1:10])
+
+# Visualización del top 10 alimentos/nutrientes para PC1:
+
+top_10_PC2
 
 
-### La salida de la línea 47 sería para la tabla 2. podemos poner los 10 primeros componentes
-### La rotación de cargas de las líneas 87 y 93 serían para la tabla 3
+# Visualizamos los valores de las cargas del top 10 A/N que más aportan pero sin valor absoluto:
+
+pca_result$rotation[top_10_PC1,1]
+pca_result$rotation[top_10_PC2,2]
 
